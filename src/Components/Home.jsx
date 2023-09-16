e28cb950fe5df2527546576e4500a65106ec8d9d
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-09-16 14:53:57 +0530

    fix card

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,7 +4,7 @@ import Photo from "../assets/photo.png";
 export default function Home() {
   const LoanCard = (recommendation) => {
     return (
-      <div className="bg-white p-2 shadow-md rounded-xl">
+      <div className="bg-white p-2 shadow rounded-xl">
         <div className="flex gap-2 items-center">
           <i
             className={`${recommendation.icon} bg-${recommendation.color}-200 p-2 rounded-lg text-${recommendation.color}-600`}
@@ -61,22 +61,22 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center md:flex-row gap-12">
-      <div className="bg-white flex flex-col justify-between p-4 rounded-3xl md:w-2/5 md:mb-28 shadow-md">
+      <div className="bg-white flex flex-col gap-6 p-4 rounded-3xl h-fit md:w-2/5 md:mb-28 shadow-md">
         <div>
           <img src={design} alt="blue and red ball" />
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center pt-6">
           <img src={Photo} alt="icon" />
         </div>
-        <div>
+        <div className="flex flex-col gap-4">
           <p className="text-3xl font-bold">Place for getting student loan</p>
-          <p>
+          <p className="text-gray-500">
             We provide the best loan offer for the student with flexible
             conditions.
           </p>
         </div>
         <div>
-          <button className="bg-[#4E58DD] text-white p-2 px-4 rounded-lg mt-6">
+          <button className="bg-[#4E58DD] text-white p-4 mb-2 px-6 rounded-xl mt-6 shadow-lg shadow-[#c2c6ff]">
             <i className="fa-solid fa-chevron-right"></i>
           </button>
         </div>
